a401416d8edf4a5cbda91ce965747d7813b97710
chengai77a6b <119432301+chengai77a6b@users.noreply.github.com>
2023-07-02 11:39:54 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+document.getElementById('messageForm').addEventListener('submit', function(event) {
+  event.preventDefault();
 
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  const mailtoLink = `mailto:chengaihhz@yeah.net?subject=新留言&body=姓名: \${name}%0A邮箱: \${email}%0A留言: \${message}`;
+
+  window.open(mailtoLink);
+});
