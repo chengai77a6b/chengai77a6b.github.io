22851282431d3713115586aba88e5bce40544d7a
chengai77a6b <119432301+chengai77a6b@users.noreply.github.com>
2023-07-02 12:39:42 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-document.getElementById('messageForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // 阻止表单提交
+// 获取表单和留言列表的引用
+const messageForm = document.getElementById('messageForm');
+const messageList = document.getElementById('messageList');
 
-    var name = document.getElementById('nameInput').value;
-    var message = document.getElementById('messageInput').value;
+// 表单提交处理函数
+function addMessage(event) {
+    event.preventDefault(); // 阻止表单默认的提交行为
 
-    // 将姓名和留言内容发送到邮箱
-    var mailtoLink = 'mailto:chengaihhz@yeah.net?subject=留言板留言&body=姓名: ' + name + '%0A留言内容: ' + message;
-    window.location.href = mailtoLink;
-});
+    // 获取输入的姓名和留言内容
+    const nameInput = document.getElementById('name');
+    const messageInput = document.getElementById('message');
+
+    const name = nameInput.value;
+    const message = messageInput.value;
+
+    // 创建新的留言元素
+    const newMessage = document.createElement('div');
+    newMessage.innerHTML = `<strong>\${name}：</strong>\${message}`;
+
+    // 将留言元素添加到留言列表
+    messageList.appendChild(newMessage);
+
+    // 清空输入框的内容
+    nameInput.value = '';
+    messageInput.value = '';
+}
+
+// 将表单提交事件绑定到处理函数
+messageForm.addEventListener('submit', addMessage);
